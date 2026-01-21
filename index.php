3566386100d1effe68bba3d8e7d1882786cadced
hodulayahweh-wq <hodulayahweh@gmail.com>
2026-01-21 14:25:11 +0300

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<h2>🔥 LORD SMM PANEL</h2>
-
-<ul>
-    <li><a href="balance.php">💰 Bakiye</a></li>
-    <li><a href="services.php">📦 Servisler</a></li>
-    <li><a href="order.php">🛒 Sipariş Gönder</a></li>
-</ul>
