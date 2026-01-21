70604b808128bd2f18a2f0987138dd4c637b9711
hodulayahweh-wq <hodulayahweh@gmail.com>
2026-01-21 14:10:45 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<h2>🔥 LORD SMM PANEL</h2>
+
+<ul>
+    <li><a href="balance.php">💰 Bakiye</a></li>
+    <li><a href="services.php">📦 Servisler</a></li>
+    <li><a href="order.php">🛒 Sipariş Gönder</a></li>
+</ul>
